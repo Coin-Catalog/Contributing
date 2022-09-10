79724371a9d186ee0e78519378affa2db16c65e0
Ethan <92686703+not-a-ethan@users.noreply.github.com>
2022-09-10 14:56:34 -0400

    Added the footer (#7)

---
+++ js/footer.js
@@ -0,0 +1,7 @@
+const home = document.getElementById('home');
+const contribute = document.getElementById('contribute');
+const gh = document.getElementById('gh');
+
+home.href = 'https://www.coincatalog.org/'
+contribute.href = 'https://contributing.coincatalog.org/'
+gh.href = 'https://github.com/Coin-Catalog'
